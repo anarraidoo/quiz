1688363026af58158acb5959a9df331ff927da80
anarraidoo <anusha.narraidoo@gmail.com>
2024-04-08 20:57:44 -0400

    Improved quiz functionality
    - Added a previous button
    - Changed the next button to a submit quiz button on reaching the last question
    - Added form validation so an answer selection is required before moving to next question or submitting the quiz
    - Improved quiz.html structure
    TODO:
    - Change radio input to buttions
    - Create storyline questions and answers
    - Add images to each question for visuals

--- quiz_start/static/script.js
+++ quiz_start/static/script.js
@@ -19,38 +19,54 @@
         selectAnswer(uid) {
             // console.log('Selected answer UID:', uid);
             this.selectedAnswers[this.currentIndex] = uid;
+//            validateForm();
         },
-        nextQuestion() {
-            this.currentIndex++;
+        nextQuestion(event) {
+            if (validateForm()) {
+                // Proceed to next question
+                event.preventDefault();
+                this.currentIndex++;
+                if (this.currentIndex == this.questions.length - 1) {
+                    document.getElementById('next').style.display = 'none';
+                    document.getElementById('submit').style.display = 'inline-block';
+                }
+            }
+        },
+        prevQuestion() {
+            this.currentIndex--;
         },
         submitQuiz() {
-            fetch('/submit-quiz/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRFToken': getCookie('csrftoken')
-                },
-                body: JSON.stringify({ answers: this.selectedAnswers })
-            })
-            .then(response => {
-                if (response.ok) {
-                    return response.json();
-                } else {
+            if (validateForm()) {
+                // Proceed to quiz submission
+                this.currentIndex++;
+                fetch('/submit-quiz/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-CSRFToken': getCookie('csrftoken')
+                    },
+                    body: JSON.stringify({ answers: this.selectedAnswers })
+                })
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        alert('Failed to submit quiz. Please try again later.');
+                    }
+                })
+                .then(data => {
+    //                document.getElementById('submit').style.display = 'none';
+    //                document.getElementById('quiz-question').style.display = 'none';
+                    this.personalityTrait = data.personality_trait;
+                    console.log('Personality Trait: ', this.personalityTrait);
+                    document.querySelector('.result-image img').src = `/static/${this.personalityTrait}.png`;
+                    document.getElementById('quiz-result').style.display = 'block';
+                })
+                .catch(error => {
+                    console.error('Error:', error);
                     alert('Failed to submit quiz. Please try again later.');
-                }
-            })
-            .then(data => {
-                document.getElementById('submit').style.display = 'none';
-                this.personalityTrait = data.personality_trait;
-                 console.log('Personality Trait: ', this.personalityTrait);
-                // alert('Quiz submitted successfully! Personality Trait: ' + personalityTrait);
-                document.querySelector('.result-image img').src = `/static/${this.personalityTrait}.png`;
-                document.getElementById('result').style.display = 'block';
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                alert('Failed to submit quiz. Please try again later.');
-            });
+                });
+            }
         }
     },
     created() {
@@ -74,3 +90,36 @@ function getCookie(name) {
     }
     return cookieValue;
 }
+
+// Function to validate the form
+function validateForm() {
+    const form = document.getElementById('quiz-form');
+    if (form.checkValidity()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// window.addEventListener('load', function () {
+//    // Check if the current page is not home.html
+//    if (this.currentIdex > 0) {
+//        prevQuestion();
+//    }
+//    if (window.location.pathname !== '') {
+//        // Add an entry to the browser's history stack
+//        window.history.pushState({ page: 'question' }, '', window.location.href);
+//    }
+//
+//    // Listen for the popstate event (triggered when the user navigates back or forward)
+//    window.addEventListener('popstate', function (event) {
+//        if this.cu
+//        // Check if the current page is home.html
+//        if (window.location.pathname === '') {
+//            // If the current page is home.html, navigate back one step in the history stack
+//            window.history.go(-1);
+//        }
+//    });
+//});
+
+
